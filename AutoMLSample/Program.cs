fcb099589d4de1579ce834be64ba88b2eb856f0d
Alan Macgowan <alanmacgowan@gmail.com>
2020-02-12 18:04:35 -0300

    initial commit

---
+++ AutoMLSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoMLSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
